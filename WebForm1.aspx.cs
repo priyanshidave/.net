fac77e404f73e2dcf88f8f06416de505bcf6d33a
priyanshidave <49427572+priyanshidave@users.noreply.github.com>
2020-03-11 09:37:26 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+using System.Xml.Xsl;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+       
+    }
+}
